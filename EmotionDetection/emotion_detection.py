f3d996c5a7cc77a1496e679bc0fdfa71a84e98a7
Balaji Sundaresan <balajisundaresan@Balajis-MacBook-Air.local>
2026-02-14 08:40:23 +0530

    updates

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,6 +6,8 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, headers = headers, json = input)
+    if response.status_code == 400:
+        return get_error_response()
     formatted_response = json.loads(response.text)
     output = {}
     emotion_score_max = 0.0
@@ -18,3 +20,13 @@ def emotion_detector(text_to_analyze):
             emotion_score_max = value
     output["dominant_emotion"] = dominant_emotion
     return output
+
+def get_error_response():
+    return {
+        'anger': None,
+        'disgust': None,
+        'fear': None,
+        'joy': None,
+        'sadness': None,
+        'dominant_emotion': None
+    }
--- server.py
+++ server.py
@@ -1,17 +1,27 @@
-from flask import Flask, render_template, request, jsonify
+"""
+This application runs a flask server for a web app to do sentiment analysis
+"""
+from flask import Flask, render_template, request
 from EmotionDetection import emotion_detection
-
 
 app = Flask(__name__)
 
 @app.route("/")
 def index_route():
+    """
+    index route to render 
+    """
     return render_template("index.html")
 
 @app.route('/emotionDetector')
 def emotion_detector():
-    textToAnalyze = request.args.get('textToAnalyze')
-    response = emotion_detection.emotion_detector(textToAnalyze)
+    """
+    returns the emotion in query string textToAnalyze passed in the url 
+    """
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detection.emotion_detector(text_to_analyze)
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
     formatted_response = (
         f"For the given statement, the system response is "
         f"'anger': {response['anger']}, "
@@ -19,7 +29,7 @@ def emotion_detector():
         f"'fear': {response['fear']}, "
         f"'joy': {response['joy']}. "
         f"The dominant emotion is  {response['dominant_emotion']}"
-    )    
+    )
     return formatted_response
 
 if __name__ == '__main__':
